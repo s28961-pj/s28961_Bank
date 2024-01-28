a189f4494f0700c0bb76c3ba0944d6d3615b7cf0
Roland Liedtke <roland-liedtke@Rolands-MacBook-Air.local>
2024-01-28 16:06:25 +0100

    add Controller layer bug fixes

--- src/main/java/com/pjatk/project/controller/AccountController.java
+++ src/main/java/com/pjatk/project/controller/AccountController.java
@@ -2,18 +2,37 @@ package com.pjatk.project.controller;
 
 import com.pjatk.project.model.Account;
 import com.pjatk.project.service.AccountService;
+import jakarta.xml.bind.ValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AccountController {
-    private AccountService accountService;
+    AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @PostMapping("/accounts")
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {
         accountService.addAccount(account);
         return ResponseEntity.ok(account);
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> getAccountList() throws ValidationException {
+        return accountService.getAccountList();
+    }
+    @GetMapping("/accounts/{id}")
+    public Account getAccountById(Integer id) {
+        return accountService.getAccountById(id);
     }
 }
