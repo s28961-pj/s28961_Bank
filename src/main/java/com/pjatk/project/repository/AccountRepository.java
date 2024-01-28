b116d87d66decb27f988a8dbca156ccdba42fed7
Roland Liedtke <roland-liedtke@Rolands-MacBook-Air.local>
2024-01-28 16:05:20 +0100

    add bug fixes in repository layer

--- src/main/java/com/pjatk/project/repository/AccountRepository.java
+++ src/main/java/com/pjatk/project/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.pjatk.project.repository;
 
 import com.pjatk.project.model.Account;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
