133ded6184709e6f393473442524dcb9e31240e7
Roland Liedtke <roland-liedtke@Rolands-MacBook-Air.local>
2024-01-28 16:05:47 +0100

    add bug fixes in Service layer

--- src/main/java/com/pjatk/project/service/AccountService.java
+++ src/main/java/com/pjatk/project/service/AccountService.java
@@ -2,13 +2,24 @@ package com.pjatk.project.service;
 
 import com.pjatk.project.model.Account;
 import com.pjatk.project.repository.AccountRepository;
+import jakarta.xml.bind.ValidationException;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
+
     private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
     public void addAccount(Account account) {
         accountRepository.addAccount(account);
@@ -18,7 +29,10 @@ public class AccountService {
         return accountRepository.getAccountById(id);
     }
 
-    public List<Account> getAccountList() {
+    public List<Account> getAccountList() throws ValidationException {
+        if(accountRepository.getAccountList().isEmpty() || accountRepository.getAccountList() == null) {
+            throw new ValidationException("Lista jest pusta");
+        } else
         return accountRepository.getAccountList();
     }
 }
